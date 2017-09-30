0c25aab114085f0f29ee8477e692cb10ac43d66b
jakeflo88 <jakeflorian.mcf@gmail.com>
2017-09-30 14:04:28 -0300

    Basic input set up.

--- isa.js
+++ isa.js
@@ -1,3 +1,15 @@
 function runGo() {
+	
+	//make sure it's running
+	var input = "";
 	console.log("running..");
+
+	//get the input text
+	input = document.getElementById("userInput").value;
+	console.log(input);
+
+	var pass = document.getElementById("portal");
+	var inputText = document.createTextNode(input);
+	pass.appendChild(inputText);
+
 }
